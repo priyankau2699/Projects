f35c912205e17dd03274fe729e78aee8bab7ff01
priyankau2699 <priyankaumeshk2699@gmail.com>
2026-09-01 22:17:53 +0530

    javascript/Task6.js

---
+++ javascript/Task6.js
@@ -0,0 +1,27 @@
+// Task 6 — Student Data
+// Create an object:
+// name
+// age
+// city
+// qualification
+// isStudent
+// Print:
+// Complete object
+// Name
+// Age
+// Qualification
+// isStudent
+let student = {
+    name: "Naveen",
+    age: 22,
+    city: "Trichy",
+    qualification: "BSc",
+    isStudent: true
+};
+
+console.log(student);
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.qualification);
+console.log(student.isStudent);

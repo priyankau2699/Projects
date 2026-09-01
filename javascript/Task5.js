288af2c27ea5f48464c560097cd42ebbaf2fafcf
priyankau2699 <priyankaumeshk2699@gmail.com>
2026-09-01 22:13:59 +0530

    javascript/Task5.js

---
+++ javascript/Task5.js
@@ -0,0 +1,26 @@
+// Task 5 — Identify Data Types
+// Create variables containing:
+// "Hello"
+// 100
+// 25.5
+// true
+// false
+// undefined
+// null
+// Use typeof and print each data type.
+
+let a = "Hello";
+let b = 100;
+let c = 25.5;
+let d = true;
+let e = false;
+let f = undefined;
+let g = null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);

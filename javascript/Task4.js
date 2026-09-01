60cad94bbefd1e379720e3f7c21d617ebcb7b17b
priyankau2699 <priyankaumeshk2699@gmail.com>
2026-09-01 21:23:54 +0530

    Task4

--- javascript/Task4.js
+++ javascript/Task4.js
@@ -1,1 +1,14 @@
-// This is task 4 
+// Task 4 — Age Calculator
+// Ask the user for their birth year.
+let birthYear= prompt("Enter your birth year:");
+// Calculate their approximate age using:
+let currentYear=2026;
+// Current Year - Birth Year
+let age = currentYear - birthYear;
+
+// Print the age in the console.
+console.log("Birth Year:", birthYear);
+console.log("Age:", age);
+// Example:
+// Birth Year: 2000
+// Age: 26

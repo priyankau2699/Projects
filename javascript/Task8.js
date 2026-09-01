04adb45d0db2f7e59689c20a8a823427e018ef6b
priyankau2699 <priyankaumeshk2699@gmail.com>
2026-09-01 22:20:56 +0530

    javascript/Task8.js

---
+++ javascript/Task8.js
@@ -0,0 +1,21 @@
+// Task 8 — Basic Calculator
+// Create two numbers:
+// let a = 20;
+// let b = 5;
+// Perform:
+// Addition
+// Subtraction
+// Multiplication
+// Division
+// Modulus
+// Exponentiation
+// Print every result separately.
+let a = 20;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);

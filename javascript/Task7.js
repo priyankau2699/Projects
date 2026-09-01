18a55ce0d51546803acb2d34b82a0c49ff5abf1a
priyankau2699 <priyankaumeshk2699@gmail.com>
2026-09-01 22:19:06 +0530

    javascript/Task7.js

---
+++ javascript/Task7.js
@@ -0,0 +1,26 @@
+// Task 7 — Fruit Array
+// Create an array containing 6 fruits.
+// Apple
+// Mango
+// Orange
+// Banana
+// Grapes
+// Papaya
+// Print:
+// First fruit
+// Second fruit
+// Last fruit
+// Total number of fruits
+let fruits = [
+    "Apple",
+    "Mango",
+    "Orange",
+    "Banana",
+    "Grapes",
+    "Papaya"
+];
+
+console.log(fruits[0]); 
+console.log(fruits[1]); 
+console.log(fruits[fruits.length - 1]); 
+console.log(fruits.length);
